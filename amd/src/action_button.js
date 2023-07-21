5c4806f91a80b17fb5d40950a3f4a651bf73f375
Georg Maißer <georg.maisser@wunderbyte.at>
2023-07-21 17:23:20 +0200

    fix js to show modal

--- amd/src/action_button.js
+++ amd/src/action_button.js
@@ -14,42 +14,62 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /*
- * @package    blocks_openai_chat
+ * @package    block_openai_chat
  * @copyright  2023 Bernhard Aichinger-Ganas & Danilo Stoilovski, wunderbyte.at <info@wunderbyte.at>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 import ModalForm from 'core_form/modalform';
 
+/**
+ * Init function.
+ */
 export function init() {
+  // eslint-disable-next-line no-console
   console.log("init");
+
   const btn = document.querySelector("#page-blocks-openai_chat-admin a.btn-openmodaladfinetuning");
+
+  // eslint-disable-next-line no-console
   console.log(btn);
   btn.addEventListener('click', e => {
+
+    // eslint-disable-next-line no-console
     console.log(e);
+
+    confirmCancelAllUsersAndSetCreditModal(e);
   });
 }
 
-export function confirmCancelAllUsersAndSetCreditModal() {
+/**
+ *
+ * @param {event} e
+ */
+function confirmCancelAllUsersAndSetCreditModal(e) {
 
-    const modalForm = new ModalForm({
+  const modalForm = new ModalForm({
 
-        // Name of the class where form is defined (must extend \core_form\dynamic_form):
-        formClass: "blocks_openai_chat\\form\\modal_input",
-        // Add as many arguments as you need, they will be passed to the form:
-    });
-    // Listen to events if you want to execute something on form submit.
-    // Event detail will contain everything the process() function returned:
-    modalForm.addEventListener(modalForm.events.FORM_SUBMITTED, (e) => {
-        window.console.log(e.detail);
+    // Name of the class where form is defined (must extend \core_form\dynamic_form):
+    formClass: "block_openai_chat\\form\\modal_input",
+    // Add as many arguments as you need, they will be passed to the form:
+    args: {},
+    // Pass any configuration settings to the modal dialogue, for example, the title:
+    modalConfig: {title: 'myform'},
+    // DOM element that should get the focus after the modal dialogue is closed:
+    returnFocus: e.currentTarget
+  });
+  // Listen to events if you want to execute something on form submit.
+  // Event detail will contain everything the process() function returned:
+  modalForm.addEventListener(modalForm.events.FORM_SUBMITTED, (e) => {
+    window.console.log(e.detail);
 
-        // Reload window after cancelling.
-        window.location.reload();
+    // Reload window after cancelling.
+    window.location.reload();
 
-        // eslint-disable-next-line no-console
-        console.log('confirmCancelAllUsersAndSetCreditModal: form submitted');
-    });
+    // eslint-disable-next-line no-console
+    console.log('confirmCancelAllUsersAndSetCreditModal: form submitted');
+  });
 
-    // Show the form.
-    modalForm.show();
+  // Show the form.
+  modalForm.show();
 }
