10236948b58e533fd9d0db8c07d7e76b0e825753
Georg Maißer <georg.maisser@wunderbyte.at>
2024-01-04 20:50:28 +0100

    Version 1.7.6 (2024010401)

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_openai_chat';
-$plugin->version = 2024010400;
+$plugin->version = 2024010401;
 $plugin->requires = 2020061513;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.7.5';
+$plugin->release = '1.7.6';
 $plugin->dependencies = [
     'local_wunderbyte_table' => 2023011200,
 ];
